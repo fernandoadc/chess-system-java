92a8bf408c0e3343d5b12a78b9ce95e14f2b93ac
fernandoadc <tinhofernando44@gmail.com>
2022-11-08 22:04:49 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /* GIT-TOKEN: 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 */
+        System.out.println("Hello, World!");
+    }
+}
