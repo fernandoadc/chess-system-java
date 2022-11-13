f5f41af4a117e1df789b8e0f1f0e4767b30792ba
fernandoadc <tinhofernando44@gmail.com>
2022-11-13 04:52:53 -0500

    starting to implement the Board and Piece classes

--- src/application/App.java
+++ src/application/App.java
@@ -4,8 +4,9 @@ import boardgame.Position;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        /* GIT-TOKEN: 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 */
+        /* GIT-TOKEN: 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 */
         Position p = new Position(2, 4);
-        System.out.println(p);
+        System.out.println(p.getRow());
+        System.out.println(p.getColumn());
     }
 }
